a13ab074da21cf68e862d5bb7213d273c67ad431
Kellen <kellen.gary@gmail.com>
2024-10-06 22:04:01 -0400

    check py

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import NavBar from "./frontEnd/Components/NavBar";
+import StockDisplay from "./frontEnd/Components/StockDisplay";
 
 function App() {
   return (
     <div className={"full-page"}>
         <NavBar />
+        <StockDisplay />
     </div>
   );
 }
---
+++ src/frontEnd/Components/StockDisplay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function StockDisplay() {
+    return (
+        <div>display</div>
+    );
+}
+
+export default StockDisplay;
